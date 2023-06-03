3a1158f30ce7c577df2a2769c43d94722cdf36ce
bodyasheva <metrolast3@gmail.com>
2023-06-03 15:22:30 +0300

    Home_work_22

---
+++ homework_12/22.py
@@ -0,0 +1,37 @@
+class Item:
+
+    def __init__(self, name, price, description, dimensions):
+        self.price = price
+        self.description = description
+        self.dimensions = dimensions
+        self.name = name
+
+    def __str__(self):
+        pass
+
+
+class User:
+
+    def __init__(self, name, surname, numberphone):
+        self.name = name
+        self.surname = surname
+        self.numberphone = numberphone
+
+    def __str__(self):
+        pass
+
+
+class Purchase:
+    def __init__(self, user):
+        self.products = {}
+        self.user = user
+        self.total = 0
+
+    def add_item(self, item, cnt):
+        self.products[item] = cnt
+
+    def __str__(self):
+        pass
+
+    def get_total(self):
+        pass
